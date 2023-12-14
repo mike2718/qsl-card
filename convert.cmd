8755775c445b5d3f3af8367893c910f9636a1062
Mike Akiba <mike2718@gmail.com>
2023-12-14 21:02:43 +0800

    修订QSL卡片内容

--- convert.cmd
+++ convert.cmd
@@ -3,6 +3,7 @@ cd /d "%~dp0"
 
 del /q *.log *.aux *.toc *.out *.pdf *.blg *.bbl *.fdb_latexmk *.fls *.xdv *.toc 2>nul
 xelatex.exe BG7XTQ.tex
-pdftoppm.exe -png -r 300 BG7XTQ.pdf > .\images\BG7XTQ.png
+del /q images\BG7XTQ.png 2>nul
+pdftoppm.exe -png -r 300 BG7XTQ.pdf > images\BG7XTQ.png
 
 pause
