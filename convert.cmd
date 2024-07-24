aa319a6c318b853eb9cb5993240f40059dfb0045
Mike Akiba <mike2718@gmail.com>
2024-07-24 17:03:30 +0800

    更新

--- convert.cmd
+++ convert.cmd
@@ -1,10 +1,9 @@
 @echo off
 cd /d "%~dp0"
 
-del /q *.log *.aux *.toc *.out *.pdf *.blg *.bbl *.fdb_latexmk *.fls *.xdv *.toc 2>nul
+del /q *.log *.aux *.toc *.out *.pdf *.blg *.bbl *.fdb_latexmk *.fls *.xdv *.toc images\*.png 2>nul
 xelatex.exe BG7XTQ.tex
 xelatex.exe BG7XTQ-custom.tex
-del /q images\BG7XTQ*.png 2>nul
 pdftoppm.exe -png -r 350 BG7XTQ.pdf > images\BG7XTQ.png
 pdftoppm.exe -png -r 350 BG7XTQ-custom.pdf > images\BG7XTQ-custom.png
 
