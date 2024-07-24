3656b3d4c447365e6868c146b170cb5b6589abbc
Mike Akiba <mike2718@gmail.com>
2024-07-24 16:36:26 +0800

    更新

--- convert.cmd
+++ convert.cmd
@@ -3,7 +3,9 @@ cd /d "%~dp0"
 
 del /q *.log *.aux *.toc *.out *.pdf *.blg *.bbl *.fdb_latexmk *.fls *.xdv *.toc 2>nul
 xelatex.exe BG7XTQ.tex
-del /q images\BG7XTQ.png 2>nul
-pdftoppm.exe -png -r 300 BG7XTQ.pdf > images\BG7XTQ.png
+xelatex.exe BG7XTQ-custom.tex
+del /q images\BG7XTQ*.png 2>nul
+pdftoppm.exe -png -r 350 BG7XTQ.pdf > images\BG7XTQ.png
+pdftoppm.exe -png -r 350 BG7XTQ-custom.pdf > images\BG7XTQ-custom.png
 
 pause
