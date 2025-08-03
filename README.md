093db6a36eb38a1133d5510f776d0c7e3b1eb2d0
Mike <mike2718@gmail.com>
2025-08-03 09:24:18 +0800

    更新设计

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# 业余无线电台 BG7XTQ 的 QSL 卡片
+# 业余无线电台 BG7XTQ 的 eQSL 卡片
 
 ## 例图
 
-![BG7XTQ的QSL卡片](images/BG7XTQ-350dpi.png)
+![业余无线电台 BG7XTQ 的 eQSL 卡片](images/BG7XTQ-example.jpg)
 
 ## 依赖
 
 - noto包
 - xeCJK包
-
+- background包
--- convert.cmd
+++ convert.cmd
@@ -1,10 +1,10 @@
 @echo off
 cd /d "%~dp0"
 
-del /q *.log *.aux *.toc *.out *.pdf *.blg *.bbl *.fdb_latexmk *.fls *.xdv *.toc images\BG7XTQ*.png 2>nul
+rem del /q *.log *.aux *.toc *.out *.pdf *.blg *.bbl *.fdb_latexmk *.fls *.xdv *.toc images\BG7XTQ*.png 2>nul
+rem xelatex.exe BG7XTQ.tex
 xelatex.exe BG7XTQ.tex
 xelatex.exe BG7XTQ-custom.tex
-pdftoppm.exe -png -r 350 BG7XTQ.pdf > images\BG7XTQ-350dpi.png
-pdftoppm.exe -png -r 350 BG7XTQ-custom.pdf > images\BG7XTQ-custom-350dpi.png
-
+magick -density 300 BG7XTQ.pdf images\BG7XTQ-example.jpg
+magick -density 300 BG7XTQ-custom.pdf images\BG7XTQ.jpg
 pause
