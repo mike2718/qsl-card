e95c96f865e951bc810bfd21de635937f4a0f102
Mike <mike2718@gmail.com>
2025-09-02 11:14:31 +0800

    update

--- convert.cmd
+++ convert.cmd
@@ -1,9 +1,9 @@
 @echo off
 cd /d "%~dp0"
 
-rem xelatex.exe BG7XTQ.tex
+xelatex.exe BG7XTQ.tex
+xelatex.exe BG7XTQ.tex
 xelatex.exe BG7XTQ-custom.tex
 xelatex.exe BG7XTQ-custom.tex
-rem magick -density 300 BG7XTQ.pdf images\BG7XTQ-example.jpg
-rem magick -density 300 BG7XTQ-custom.pdf images\BG7XTQ.jpg
+magick -density 300 BG7XTQ.pdf images\BG7XTQ-example.jpg
 pause
