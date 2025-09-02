aead627cb953740ca53adc2967de8a5d831e439c
Mike <mike2718@gmail.com>
2025-09-02 11:11:14 +0800

    update

--- convert.cmd
+++ convert.cmd
@@ -1,10 +1,9 @@
 @echo off
 cd /d "%~dp0"
 
-rem del /q *.log *.aux *.toc *.out *.pdf *.blg *.bbl *.fdb_latexmk *.fls *.xdv *.toc images\BG7XTQ*.png 2>nul
 rem xelatex.exe BG7XTQ.tex
-xelatex.exe BG7XTQ.tex
+xelatex.exe BG7XTQ-custom.tex
 xelatex.exe BG7XTQ-custom.tex
 rem magick -density 300 BG7XTQ.pdf images\BG7XTQ-example.jpg
-magick -density 300 BG7XTQ-custom.pdf images\BG7XTQ.jpg
+rem magick -density 300 BG7XTQ-custom.pdf images\BG7XTQ.jpg
 pause
